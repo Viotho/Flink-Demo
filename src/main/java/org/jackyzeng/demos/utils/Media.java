277d3ce4cfe6f7427b5345b7ddcc16be9cf28bba
Jacky Zeng <jinzengjacky@qq.com>
2021-08-06 15:11:10 +0800

    Add media source.

--- src/main/java/org/jackyzeng/demos/utils/Media.java
+++ src/main/java/org/jackyzeng/demos/utils/Media.java
@@ -1,8 +1,14 @@
 package org.jackyzeng.demos.utils;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Media {
+    private String symbol;
+    private long timestamp;
     private String Status;
 }
