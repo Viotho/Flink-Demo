71584bd7dd7b880843c688f251ff2e84a3cb6582
Jacky Zeng <jinzengjacky@qq.com>
2021-08-10 17:02:16 +0800

    Update interval join demo.

--- src/main/java/org/jackyzeng/demos/functions/IntervalJoinDemo.java
+++ src/main/java/org/jackyzeng/demos/functions/IntervalJoinDemo.java
@@ -1,5 +1,6 @@
 package org.jackyzeng.demos.functions;
 
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
@@ -21,8 +22,13 @@ public class IntervalJoinDemo {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         DataStreamSource<Tuple3<String, Long, Integer>> input1 = env.fromElements(Tuple3.of("One",System.currentTimeMillis(), 1), Tuple3.of("Two", System.currentTimeMillis() ,2));
         DataStreamSource<Tuple3<String, Long, Integer>> input2 = env.fromElements(Tuple3.of("Three", System.currentTimeMillis(), 3), Tuple3.of("Four", System.currentTimeMillis(), 4));
-        SingleOutputStreamOperator<String> intervalJoinStream = input1.keyBy(element1 -> element1.f0)
-                .intervalJoin(input2.keyBy(element2 -> element2.f0))
+
+        SingleOutputStreamOperator<String> intervalJoinStream = input1.assignTimestampsAndWatermarks(WatermarkStrategy.<Tuple3<String, Long, Integer>>forMonotonousTimestamps()
+                        .withTimestampAssigner((element, timestamp) -> element.f1))
+                .keyBy(element1 -> element1.f0)
+                .intervalJoin(input2.assignTimestampsAndWatermarks(WatermarkStrategy.<Tuple3<String, Long, Integer>>forMonotonousTimestamps()
+                                .withTimestampAssigner((element, timestamp) -> element.f1))
+                        .keyBy(element2 -> element2.f0))
                 .between(Time.milliseconds(-5), Time.milliseconds(10))
                 .process(new CustomJoinFunction());
 
