7326c80fb324b974dfaf13a0da60c38c04b49375
Jacky Zeng <jinzengjacky@qq.com>
2021-08-12 09:42:52 +0800

    Update utils.

---
+++ src/main/java/org/jackyzeng/demos/utils/BehaviourPattern.java
@@ -0,0 +1,13 @@
+package org.jackyzeng.demos.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BehaviourPattern {
+    public String firstBehaviour;
+    public String secondBehaviour;
+}
