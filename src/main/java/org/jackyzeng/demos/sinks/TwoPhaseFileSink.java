dc8ca5afee40940d57ee5bacd691b13dceb66fb2
Jacky Zeng <jinzengjacky@qq.com>
2021-08-13 14:33:01 +0800

    Add two phase commit sink function demo.

--- src/main/java/org/jackyzeng/demos/sinks/TwoPhaseFileSink.java
+++ src/main/java/org/jackyzeng/demos/sinks/TwoPhaseFileSink.java
@@ -6,6 +6,11 @@ import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.functions.sink.TwoPhaseCommitSinkFunction;
 
 import java.io.BufferedWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class TwoPhaseFileSink extends TwoPhaseCommitSinkFunction<Tuple2<String, Integer>, String, Void> {
 
@@ -21,26 +26,48 @@ public class TwoPhaseFileSink extends TwoPhaseCommitSinkFunction<Tuple2<String, 
 
     @Override
     protected void invoke(String transaction, Tuple2<String, Integer> input, Context context) throws Exception {
-        
+        transactionWriter.write(input.f0 + " " + input.f1 + "\n");
     }
 
     @Override
     protected String beginTransaction() throws Exception {
-        return null;
+        String time = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        int subTaskInx = getRuntimeContext().getIndexOfThisSubtask();
+        String fileName = time + "-" + subTaskInx;
+        Path preCommitFilePath = Paths.get(preCommitPath + "/" + fileName);
+        Files.createFile(preCommitFilePath);
+        transactionWriter = Files.newBufferedWriter(preCommitFilePath);
+        return fileName;
     }
 
     @Override
     protected void preCommit(String transaction) throws Exception {
-
+        transactionWriter.flush();
+        transactionWriter.close();
     }
 
     @Override
     protected void commit(String transaction) {
-
+        Path preCommitFilePath = Paths.get(preCommitPath + "/" + transaction);
+        if (Files.exists(preCommitFilePath)) {;
+            Path committedPath = Paths.get(this.committedPath + "/" + transaction);
+            try {
+                Files.move(preCommitFilePath, committedPath);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
     }
 
     @Override
     protected void abort(String transaction) {
-
+        Path preCommitFilePath = Paths.get(preCommitPath + "/" + transaction);
+        if (Files.exists(preCommitFilePath)) {
+            try {
+                Files.delete(preCommitFilePath);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
     }
 }
