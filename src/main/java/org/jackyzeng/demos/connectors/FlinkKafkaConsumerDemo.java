dbe896faaca3eeda663c674c8e5bb9a17b2dfdff
Jacky Zeng <jinzengjacky@qq.com>
2021-09-01 23:31:36 +0800

    Update flink kafka connector demos.

--- src/main/java/org/jackyzeng/demos/connectors/FlinkKafkaConsumerDemo.java
+++ src/main/java/org/jackyzeng/demos/connectors/FlinkKafkaConsumerDemo.java
@@ -4,6 +4,7 @@ import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 
 import java.util.Properties;
 
@@ -11,8 +12,8 @@ public class FlinkKafkaConsumerDemo {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         Properties properties = new Properties();
-        properties.put("bootstrap.servers", "localhost:9092");
-        properties.put("group.id", "flink-group");
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "flink-group");
         String topic = "topic";
         FlinkKafkaConsumer<String> consumer = new FlinkKafkaConsumer<>(topic, new SimpleStringSchema(), properties);
         consumer.setStartFromEarliest();
--- src/main/java/org/jackyzeng/demos/connectors/FlinkKafkaProducerDemo.java
+++ src/main/java/org/jackyzeng/demos/connectors/FlinkKafkaProducerDemo.java
@@ -4,6 +4,7 @@ import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.jackyzeng.demos.connectors.serialization.KafkaWordCountSerializationSchema;
 
 import java.util.Properties;
@@ -13,7 +14,7 @@ public class FlinkKafkaProducerDemo {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         DataStreamSource<Tuple2<String, Integer>> wordCount = env.fromElements(Tuple2.of("hello", 1), Tuple2.of("world", 1));
         Properties properties = new Properties();
-        properties.put("bootstrap.servers", "localhost:9092");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         String topic = "topic";
         FlinkKafkaProducer<Tuple2<String, Integer>> producer = new FlinkKafkaProducer<>(topic, new KafkaWordCountSerializationSchema(topic),
                 properties, FlinkKafkaProducer.Semantic.EXACTLY_ONCE);
