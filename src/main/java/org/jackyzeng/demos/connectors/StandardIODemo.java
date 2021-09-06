1f61448d234cc73318e334ff726bfeff2768376c
Jacky Zeng <jinzengjacky@qq.com>
2021-09-06 23:45:53 +0800

    Update flink file IO demos.

--- src/main/java/org/jackyzeng/demos/connectors/StandardIODemo.java
+++ src/main/java/org/jackyzeng/demos/connectors/StandardIODemo.java
@@ -1,16 +1,23 @@
 package org.jackyzeng.demos.connectors;
 
 import org.apache.flink.api.common.ExecutionConfig;
+import org.apache.flink.api.common.serialization.Encoder;
+import org.apache.flink.api.common.serialization.SimpleStringEncoder;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.io.TextInputFormat;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink;
 import org.apache.flink.streaming.api.functions.source.FileProcessingMode;
 import org.jackyzeng.demos.utils.StockPrice;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class StandardIODemo {
@@ -46,7 +53,16 @@ public class StandardIODemo {
         DataStreamSource<String> fileStream = env.readTextFile(filePath);
         // Read file with FileInputFormat.
         TextInputFormat textInputFormat = new TextInputFormat(new Path(filePath));
-        env.readFile(textInputFormat, filePath, FileProcessingMode.PROCESS_CONTINUOUSLY, 100);
+        DataStreamSource<String> file = env.readFile(textInputFormat, filePath, FileProcessingMode.PROCESS_CONTINUOUSLY, 100);
+        StreamingFileSink<String> fileSink = StreamingFileSink.forRowFormat(new Path("path/to/file"), new SimpleStringEncoder<String>("UTF-8")).build();
+        file.addSink(fileSink);
+    }
 
+    static class Tuple2Encoder implements Encoder<Tuple2<String, Integer>> {
+        @Override
+        public void encode(Tuple2<String, Integer> element, OutputStream outputStream) throws IOException {
+            outputStream.write((element.f0 + "@" + element.f1).getBytes(StandardCharsets.UTF_8));
+            outputStream.write('\n');
+        }
     }
 }
